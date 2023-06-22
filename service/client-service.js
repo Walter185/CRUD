98adcdf3a2f8bcb69a98a0dfa0677df0a8b1648d
Walter Liendo <walter.liendo@gmail.com>
2023-06-22 10:45:21 -0300

    minimizando la conexion al server

--- service/client-service.js
+++ service/client-service.js
@@ -38,24 +38,8 @@ const table = document.querySelector("[data-table]");
 // Update - PUT/PATCH
 // Delete - DELETE
 
-const listaClientes = () => {
-  const promise = new Promise((resolve, reject) => {
-    const http = new XMLHttpRequest();
-    http.open("GET", "http://localhost:3000/perfiles");
-
-    http.send();
-
-    http.onload = () => {
-      const response = JSON.parse(http.response);
-      if (http.status >= 400) {
-        reject(response);
-      } else {
-        resolve(response);
-      }
-    };
-  });
-  return promise;
-};
+const listaClientes = () => fetch("http://localhost:3000/perfil").then((respuesta) => 
+     respuesta.json());
 
 listaClientes()
   .then((data) => {
